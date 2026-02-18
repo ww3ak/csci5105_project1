5a796d19cf1ad4e9aaf1b0ec04212476a1041196
Sabrina Simkhovich <sabrinasimkhovich@gmail.com>
2026-02-18 19:52:22 +0000

    added test bench functions and a project readme

---
+++ PROJECT_README.md
@@ -0,0 +1,35 @@
+# Project 1 CSCI 5105
+
+**Distributed gRPC Key-Value Store with Textbook Chunks and Embeddings**
+
+---
+## Authors
+
+- Sabrina Simkhovich (simkh005@umn.edu)
+- 
+
+
+## Project Structure
+
+
+## How to Run Project
+
+
+## Running Tests
+
+Unit tests for the gRPC handlers are located in the `testBench/` directory and use Python's built-in `unittest` framework.
+
+### Steps to Run Tests
+
+1. **Navigate to the project root** (the directory containing `server/`, `testBench/`, and `gRPC_KVS/`):
+
+```bash
+cd /workspaces/project_1
+```
+
+2. **Run tests using Python’s unittest module**
+
+```bash
+python -m unittest testBench.test_kv_service
+```
+
--- testBench/test_kv_service.py
+++ testBench/test_kv_service.py
@@ -1,22 +1,48 @@
 import unittest
+from server.server import InMemoryKV
+from types import SimpleNamespace
 
-#one thing i am unsure of is do we need to spin up  channel and stub
-#for each test? or can we just fake it by doing something like this: 
-        # self.service.textbook_chunks = {
-        #     "k1": "hello world"
-        # }
 
-class TestGetText(unittest.TestCase):
-    def test_existing_key(self):
-        # should return key value
+class gRPCTestSetup(unittest.TestCase):
+    def setUp(self):
+        self.service = InMemoryKV()
+        self.service.textbook_chunks = {}
+        #not sure if embeddings are necessary
+        self.service.embeddings = {}
+
+class TestPutText(gRPCTestSetup):
+    def test_put_existing_key(self):        
         pass
+
+    def test_put_new_key(self):
+
+        pass
+class TestGetText(gRPCTestSetup):
+
+    def test_existing_key(self):
+        self.service.textbook_chunks["test_key"] = "test_value"
+
+        #fake request key
+        request = SimpleNamespace(key="test_key")
+        #get GetText response 
+        response = self.service.GetText(request)
+        # response should be true and return test_value
+        self.assertTrue(response.found)
+        self.assertEqual(response.textbook_chunk, "test_value")
 
     def test_missing_key(self):
-        # should return not found
-        pass
+        #fake request key
+        request = SimpleNamespace(key="missing_key")
+
+        #get GetText response 
+        response = self.service.GetText(request)
+
+        # respose should be false and return empty string
+        self.assertFalse(response.found)
+        self.assertEqual(response.textbook_chunk, "")
 
 
-class TestDeleteKey(unittest.TestCase):
+class TestDeleteKey(gRPCTestSetup):
     def test_delete_existing_key(self):
         pass
 
@@ -24,11 +50,11 @@ class TestDeleteKey(unittest.TestCase):
         pass
 
 
-class TestListKey(unittest.TestCase):
+class TestListKey(gRPCTestSetup):
     pass
 
 
-class TestHealth(unittest.TestCase):
+class TestHealth(gRPCTestSetup):
     pass
 
 
